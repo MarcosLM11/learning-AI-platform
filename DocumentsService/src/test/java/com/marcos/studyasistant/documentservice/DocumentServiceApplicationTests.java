f7f0588eb094206b909652c3f9cff073535f9568
marcos <marcoslopezmarinn@gmail.com>
2025-06-17 15:20:23 +0200

    feat: add bootstrap configuration for documents service and fix fail-fast typo

---
+++ DocumentsService/src/test/java/com/marcos/studyasistant/documentservice/DocumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.studyasistant.documentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
